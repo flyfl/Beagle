9829e9efa5071af2e56d46671d0c398f0911f0d3
Flyfl <ansgar.spiegler@gmail.com>
2016-01-10 22:41:50 +0100

    Added javadoc to finalJudge

--- Core/src/main/java/de/uka/ipd/sdq/beagle/core/judge/FinalJudge.java
+++ Core/src/main/java/de/uka/ipd/sdq/beagle/core/judge/FinalJudge.java
@@ -1,18 +1,28 @@
 package de.uka.ipd.sdq.beagle.core.judge;
 
-/*
- * ATTENTION: Checkstyle turned off! remove this comment block when adding details to this
- * interface!
- *
- * CHECKSTYLE:OFF
- *
- * TODO
- */
+import de.uka.ipd.sdq.beagle.core.Blackboard;
 
 /**
- * TODO Document this type.
+ * As last running entity of Beagle's core, the FinalJudge adopts two tasks.
+ * Firstly, it decides for each {@link MeasureableSeffElement} which available {@link EvaluableExpression} from the
+ * {@link Blackboard} will be taken as final version, marking it as the currently best fitting expression.
+ * Secondly, it gives response if the analysing process has reached an end or should keep going on.
+ * @author Ansgar Spiegler
  */
-public interface FinalJudge {
+public class FinalJudge {
 
-	void judge(FinalJudgeBlackboardView blackboard);
+	/**
+	 * The judge function is responsible for setting the final expression of
+	 * {@link MeasurableSeffElement MeasurableSeffElements}.
+	 * Therefore it calls {@link EvaluableExpressionFitnessFunction#grade} on the blackboard, with SEFF elements and
+	 * EvaluableExpressions as parameters and chooses the one that has the lowest return value as final Expression.
+	 *
+	 * @param blackboard The current Blackboard
+	 * @return {@code True} if the analysing process should keep going on, {@code false} if not.
+	 */
+	public boolean judge(final Blackboard blackboard) {
+		return false;
+	}
+	
+	
 }
